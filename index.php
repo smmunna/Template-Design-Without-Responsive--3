5b06e068a9342ebf12a2d5a5177eafcd8174a4b4
Minhazul Abedin Munna <64527538+smmunna@users.noreply.github.com>
2022-05-17 20:36:40 +0600

    commited1

---
+++ index.php
@@ -0,0 +1,256 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="jscss/meanmenu.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+    <link rel="stylesheet" href="style/main.css">
+    <link rel="stylesheet" href="style/responsive.css">
+    <title>OS Template</title>
+  </head>
+  <body>
+    <!-- 1st header -->
+
+        <div id="mobile_menu"></div>
+
+            <section>
+                <div class="sec1">
+                    <div class="head1">
+                        <div class="row row1">
+                            <div class="col-lg-6">
+                                <ul class="list1">
+                                    <li><a href="#">Sign Up</a></li>
+                                    <li><a href="#">Login</a></li>
+                                    <li><a href="#">RSS Feed</a></li>
+                                    <li><a href="#">Archieved News</a></li>
+                                </ul>
+                            </div>
+                            <div class="col-lg-6">
+                                <div class="inp">
+                                    <input id="search" type="text" placeholder="Search here">
+                                        <svg xmlns="http://www.w3.org/2000/svg"  id="sbox" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+                                             <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+                                        </svg>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="container">
+                     <div class="head2">
+                            <div class="row">
+                                    <div class="col-lg-6">
+                                        <div class="logo">
+                                            <p class="log1">SYSTEMATIC</p>
+                                            <p class="log2">Free PSD Website Template</p>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-6">
+                                        <div class="icons">
+                                            <a href="#"><i class="fa-brands icn fb fa-facebook"></i></a>
+                                            <a href="#"><i class="fa-brands icn tw fa-twitter"></i></a>
+                                            <a href="#"><i class="fa-brands icn pin fa-pinterest"></i></a>
+                                            <a href="#"><i class="fa-brands icn gg fa-google-plus"></i></a>
+                                            <a href="#"><i class="fa-solid icn rs fa-square-rss"></i></a>
+                                        </div>
+                                    </div>
+                            </div>
+                     </div>
+                        <div class="head3">
+                            <!-- Navbar will be start here so check it... -->
+                            <div class="mynav">
+                                <div class="col-lg-12">
+                                <nav id="main_menu" class="navbar navbar-expand-lg  navbar-light">
+                                    <div class="container-fluid">
+                                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+                                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                                        <ul class="navbar-nav mynav1 me-auto mb-2 mb-lg-0">
+                                            <li class="nav-item">
+                                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                                            </li>
+                                            <li class="nav-item">
+                                            <a class="nav-link" href="#">Style Demo</a>
+                                            </li>
+                                            <li class="nav-item">
+                                            <a class="nav-link" href="#">Full Width</a>
+                                            </li>
+                                            <li class="nav-item dropdown">
+                                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                                Dropdown
+                                            </a>
+                                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                                <li><a class="dropdown-item" href="#">Action</a></li>
+                                                <li><a class="dropdown-item" href="#">Another action</a></li>
+                                                <li><hr class="dropdown-divider"></li>
+                                                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                                            </ul>
+                                            </li>
+                                            <li class="nav-item">
+                                            <a class="nav-link ">Gallery</a>
+                                            </li>
+                                            <li class="nav-item">
+                                            <a class="nav-link ">Portfolio</a>
+                                            </li>
+                                            <li class="nav-item">
+                                            <a class="nav-link ">Link Text</a>
+                                            </li>
+                                            <li class="nav-item">
+                                            <a class="nav-link "> Long Link Text</a>
+                                            </li>
+                                        </ul>
+                                        
+                                        </div>
+                                    </div>
+                                    </nav>
+                                </div>
+                            </div>
+                        </div>
+
+                        <!-- Main container will start here -->
+                        <div class="sec2">
+                            <div class="row">
+                                <div class="col-lg-4">
+                                    <div class="row justify-content-center">
+                                        <div class="col-4 img1">
+                                            <p class="txt1">400 x 440px</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-8">
+                                    <p class="txt2">A HUGE <br> TITLE HERE</p>
+                                    <p class="txt3">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id aliquid hic tempore amet ratione ipsam iure dolores voluptatem excepturi odit recusandae dolor, voluptatibus eos. Quam consequuntur architecto voluptatibus cumque maiores!</p>
+                                    <a href="#" class="link1">Read More »</a>
+                                </div>
+                            </div>
+                        </div>
+
+                        
+                        
+
+                    </div>
+
+                        <!-- Outside of the container there will be 3 column -->
+                        <div class="sec3">
+                            <div class="row">
+                                <div class="col-lg-4">
+                                    <p class="txt4">A Little About Us</p>
+                                        <div class="mg2">
+                                            <p class="txt6">290 x 140px</p>
+                                        </div>
+                                    <p class="txt5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque tempora modi optio vero veniam! Soluta, at quisquam. Culpa praesentium nobis quae. Dolor deleniti, repellat nam culpa iste veritatis esse optio deserunt commodi iusto neque voluptatibus quisquam quibusdam laborum. Magni, suscipit?</p>
+                                    <a href="" class="link2">Read More About US »</a>
+                                </div>
+                                <div class="col-lg-4">
+                                    <p class="txt4">Some of Our Services</p>
+                                    <div class="row">
+                                        <div class="col-lg-6">
+                                            <div class="mg3">
+                                                <p class="txt7">64 x 64px</p>
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-6">
+                                            <div class="rght">
+                                                <p class="txt8">Service Name</p>
+                                                <p class="txt9">Inteligula congue id elis donec sce sag ittis intes id laoreet aenean. </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <!-- Same thing1 -->
+                                    <div class="row">
+                                        <div class="col-lg-6">
+                                            <div class="mg3">
+                                                <p class="txt7">64 x 64px</p>
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-6">
+                                            <div class="rght">
+                                                <p class="txt8">Service Name</p>
+                                                <p class="txt9">Inteligula congue id elis donec sce sag ittis intes id laoreet aenean. </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <!-- same thing2 -->
+                                    <div class="row">
+                                        <div class="col-lg-6">
+                                            <div class="mg3">
+                                                <p class="txt7">64 x 64px</p>
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-6">
+                                            <div class="rght">
+                                                <p class="txt8">Service Name</p>
+                                                <p class="txt9">Inteligula congue id elis donec sce sag ittis intes id laoreet aenean. </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <!-- Same thing3 -->
+                                    <div class="row">
+                                        <div class="col-lg-6">
+                                            <div class="mg3">
+                                                <p class="txt7">64 x 64px</p>
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-6">
+                                            <div class="rght">
+                                                <p class="txt8">Service Name</p>
+                                                <p class="txt9">Inteligula congue id elis donec sce sag ittis intes id laoreet aenean. </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <a href="#" class="link3">Read More About US »</a>
+                                </div>
+                                    
+                                <div class="col-lg-4">
+                                    <p class="txt4">What Our Clients Say</p>
+                                    <div class="cmmnt">
+                                        <div class="qte">
+                                            <svg xmlns="http://www.w3.org/2000/svg" class="qteicon" fill="currentColor" class="bi bi-quote" viewBox="0 0 16 16">
+                                                                <path d="M12 12a1 1 0 0 0 1-1V8.558a1 1 0 0 0-1-1h-1.388c0-.351.021-.703.062-1.054.062-.372.166-.703.31-.992.145-.29.331-.517.559-.683.227-.186.516-.279.868-.279V3c-.579 0-1.085.124-1.52.372a3.322 3.322 0 0 0-1.085.992 4.92 4.92 0 0 0-.62 1.458A7.712 7.712 0 0 0 9 7.558V11a1 1 0 0 0 1 1h2Zm-6 0a1 1 0 0 0 1-1V8.558a1 1 0 0 0-1-1H4.612c0-.351.021-.703.062-1.054.062-.372.166-.703.31-.992.145-.29.331-.517.559-.683.227-.186.516-.279.868-.279V3c-.579 0-1.085.124-1.52.372a3.322 3.322 0 0 0-1.085.992 4.92 4.92 0 0 0-.62 1.458A7.712 7.712 0 0 0 3 7.558V11a1 1 0 0 0 1 1h2Z"/>
+                                            </svg>
+                                        </div>
+                                        <div class="txt10">
+                                            Tommy Tanker - CEO
+                                        </div>
+                                        <div class="txt11">
+                                        Justoid nonummy Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste, omnis. laoreet phasellent penatoque in antesque pellus elis eget tincidunt. Nequatdui laorem justo a non tellus laoreet tincidunt ut vel velit. Idenim semper pellente velis felit ac nullam pretium morbi lacus.
+                                        </div>
+                                        <a href="#" class="link4">View this Project »</a>
+                                    </div>
+                                    
+                                    <!-- Copy the same thing -->
+                                    <div class="cmmnt1">
+                                        <div class="qte">
+                                            <svg xmlns="http://www.w3.org/2000/svg" class="qteicon" fill="currentColor" class="bi bi-quote" viewBox="0 0 16 16">
+                                                                <path d="M12 12a1 1 0 0 0 1-1V8.558a1 1 0 0 0-1-1h-1.388c0-.351.021-.703.062-1.054.062-.372.166-.703.31-.992.145-.29.331-.517.559-.683.227-.186.516-.279.868-.279V3c-.579 0-1.085.124-1.52.372a3.322 3.322 0 0 0-1.085.992 4.92 4.92 0 0 0-.62 1.458A7.712 7.712 0 0 0 9 7.558V11a1 1 0 0 0 1 1h2Zm-6 0a1 1 0 0 0 1-1V8.558a1 1 0 0 0-1-1H4.612c0-.351.021-.703.062-1.054.062-.372.166-.703.31-.992.145-.29.331-.517.559-.683.227-.186.516-.279.868-.279V3c-.579 0-1.085.124-1.52.372a3.322 3.322 0 0 0-1.085.992 4.92 4.92 0 0 0-.62 1.458A7.712 7.712 0 0 0 3 7.558V11a1 1 0 0 0 1 1h2Z"/>
+                                            </svg>
+                                        </div>
+                                        <div class="txt10">
+                                            Tommy Tanker - CEO
+                                        </div>
+                                        <div class="txt11">
+                                        Justoid Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic, laboriosam! nonummy laoreet phasellent penatoque in antesque pellus elis eget tincidunt. Nequatdui laorem justo a non tellus laoreet tincidunt ut vel velit. Idenim semper pellente velis felit ac nullam pretium morbi lacus.
+                                        </div>
+                                        <a href="#" class="link4">View this Project »</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                </div>
+            </section>
+
+
+
+            <!-- Option 1: Bootstrap Bundle with Popper -->
+            <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+            <script src="jscss/meanmenu.js"></script>
+            <script src="jscss/main.js"></script>
+    </body>
+</html>
